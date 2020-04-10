e0c6fd2872227e0a1b2e06f65b7564b76d4dd3f1
hhhyg <31395326+hhhyg@users.noreply.github.com>
2020-04-10 15:58:19 +0800

    .

--- yunbanke/src/main/java/com/gcsj/yunbanke/Service/ShortMessageService.java
+++
@@ -1,7 +0,0 @@
-package com.gcsj.yunbanke.Service;
-
-public interface ShortMessageService {
-    Integer sendEmailMessage(String account ,String receiver, String purpose);
-
-    Integer verifyEmailMessage(String code, String account, String email);
-}
